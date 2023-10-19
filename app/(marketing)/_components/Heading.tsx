22d1320116f43593fc3c904ffd128ade53fb34df
one_punch_man <ckim@banlaw.com>
2023-10-19 21:33:29 +1100

    build environment settup

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Idea, Documents, & Plans, Unified. Welcome to{" "}
+        <span className="underline">Jotion</span>
+      </h1>
+    </div>
+  );
+};
